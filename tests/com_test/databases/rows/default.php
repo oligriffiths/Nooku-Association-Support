aab1224ed7898a649294155aaefa24d64cc82a97
Oli Griffiths <oli@organic-development.com>
2013-01-21 13:42:03 +0000

    Removing files

--- tests/com_test/databases/rows/default.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class ComTestDatabaseRowDefault extends KDatabaseRowActiveRecord
-{
-
-}
--- tests/com_test/databases/tables/default.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class ComTestDatabaseTableDefault extends KDatabaseTableActiveRecord
-{
-
-}
